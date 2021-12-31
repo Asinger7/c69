51a1af04e0dbe373b1e525dab1db2d619beb09fa
Asinger7 <96913785+Asinger7@users.noreply.github.com>
2021-12-31 18:29:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+name_array=[];
+function submit(){
+
+    var name_1=document.getElementById("name1").value ;
+    var name_2=document.getElementById("name2").value ;
+    var name_3=document.getElementById("name1").value ;
+    var name_4=document.getElementById("name4").value ;
+    var name_5=document.getElementById("name5").value ;
+
+    name_array.push(name_1);
+    name_array.push(name_2);
+    name_array.push(name_3);
+    name_array.push(name_4);
+    name_array.push(name_5);
+ console.log
+
